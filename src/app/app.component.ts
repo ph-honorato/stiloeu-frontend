d01eb0dc360bf7e64ff3f6979b0d2081494a5060
Pedro Honorato <ph.honorato28@gmail.com>
2022-01-24 17:23:59 -0300

    Construtor, OnInit e Click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'stiloeu-frontend';
+export class AppComponent implements OnInit {
+  
+
+  /*
+    Variáveis
+  */
+  public count = 0;
+
+
+  /*
+    Construtor e OnInit
+  */
+  constructor(
+    // private cs: CommonService
+  ){ }
+
+  ngOnInit() { }
+
+
+  
+  add(){
+
+    this.count = this.count + 1;
+
+  }
+
+
+
 }
+
+
